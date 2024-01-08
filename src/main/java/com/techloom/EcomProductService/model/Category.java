4cf44f313505496231ac1578fc865bf1d5cba58e
Shivappa Sangapur <shivappasangapur@Shivappas-MacBook-Pro.local>
2024-01-08 09:14:08 +0530

    Added repository layer

---
+++ src/main/java/com/techloom/EcomProductService/model/Category.java
@@ -0,0 +1,12 @@
+package com.techloom.EcomProductService.model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Category extends BaseModel{
+    private String categoryName;
+}
--- src/main/java/com/techloom/EcomProductService/model/Product.java
+++ src/main/java/com/techloom/EcomProductService/model/Product.java
@@ -1,16 +1,27 @@
 package com.techloom.EcomProductService.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    private int id;
+@Entity
+public class Product extends BaseModel{
     private String title;
     private double price;
-    private String category;
     private String description;
     private String image;
 
+    @ManyToOne
+    private Category category;
+
 }
+
+/**
+  Product  -> Category : M:1
+   1             1
+   M             1
+   M             1
+ */
