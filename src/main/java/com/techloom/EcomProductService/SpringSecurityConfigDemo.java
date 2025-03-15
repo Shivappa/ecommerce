04e6113a2250706909496c5fb2add184b378ec07
Shivappa Sangapur <ssangapur3@gmail.com>
2025-03-15 08:22:16 +0530

    Added user authentication service

--- src/main/java/com/techloom/EcomProductService/SpringSecurityConfigDemo.java
+++ src/main/java/com/techloom/EcomProductService/SpringSecurityConfigDemo.java
@@ -1,4 +1,4 @@
-package com.techloom.EcomProductService.userService.service;
+package com.techloom.EcomProductService;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
