628ca097c996084192d8871fb8f7473326ba58bb
Shivappa Sangapur <ssangapur3@gmail.com>
2025-03-16 07:31:50 +0530

    Integrate Payment gateway service

--- src/main/java/com/techloom/EcomProductService/paymentService/paymentgateways/PaymentGateway.java
+++ src/main/java/com/techloom/EcomProductService/paymentService/paymentgateways/PaymentGateway.java
@@ -1,5 +1,8 @@
 package com.techloom.EcomProductService.paymentService.paymentgateways;
 
+import com.razorpay.RazorpayException;
+import com.stripe.exception.StripeException;
+
 public interface PaymentGateway {
     String generatePaymentLink(Long orderId, Long amount, String phoneNumber) throws RazorpayException, StripeException;
 }
